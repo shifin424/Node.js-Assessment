116f356f068b9ecc2232ed1722cec731c1081739
shifin424 <snehamparakette004@gmail.com>
2023-11-20 13:31:59 +0530

    created the file structure

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config()
+import morgan from 'morgan';
+import cors from './middleware/security/cors.js';
+import connectionDatabase from './config/database.js'
+import router from "./routes/productRoutes.js"
+import errorHandler from "./middleware/error/errorHandler.js"
+
+const app = express();
+
+app.use(cors)
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(morgan("dev"))
+
+app.use('/api/v1',router)
+app.use(errorHandler)
+
+app.use((res,req)=>{
+    res.status(400).json({success:false, status:404,message:"Not Found"})
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port,()=>{
+    console.log(`The server conncetion is now established and running on the port ${port}`)
+})
